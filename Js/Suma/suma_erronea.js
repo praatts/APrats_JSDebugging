3b54b4f12b5c9dc1fbcc92535fabe73ccaf086be
praatts <127567514+praatts@users.noreply.github.com>
2024-12-09 19:25:43 +0100

    Ejercicio Suma corregido

--- Js/Suma/suma_erronea.js
+++ Js/Suma/suma_erronea.js
@@ -1,6 +1,6 @@
 function calcularSuma() {
-    const num1 = document.getElementById('num1').value;
-    const num2 = document.getElementById('num2').value;
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
     const suma = num1 + num2; 
     document.getElementById('resultado').innerText = `La suma es: ${suma}`;
 }
