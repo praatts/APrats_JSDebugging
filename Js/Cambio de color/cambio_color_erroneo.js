4bf5da2bbb59ca1c74e7205aebbdf1b7b1a50deb
praatts <127567514+praatts@users.noreply.github.com>
2024-12-09 18:59:46 +0100

    Ejercicio Cambio Color corregido

--- Js/Cambio de color/cambio_color_erroneo.js
+++ Js/Cambio de color/cambio_color_erroneo.js
@@ -1,5 +1,5 @@
 function cambiarColor() {
     const colores = ["red", "blue", "green", "yellow", "purple"];
-    const indice = Math.random() * colores.length; 
+    const indice = Math.floor(Math.random() * colores.length); 
     document.body.style.backgroundColor = colores[indice];
 }
